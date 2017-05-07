7a4ee990ebb06997ba829b0d7a02384793ca1ac5
Daniel Micay <danielmicay@gmail.com>
2017-05-07 00:43:03 -0400

    make sysctl constants read-only
    Most of this is extracted from the last publicly available version of
    the PaX patches where it's part of KERNEXEC as __read_only. It has been
    extended to a few more of these constants.
    
    maade: hung_task_timeout_max was moved to kernel/hung_task.c
    
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
    Signed-off-by: randomhydrosol <randomhydrosol@glassrom.org>
    Change-Id: I51c602675f67db0ba5257c3f4460fdd29517a5f1
    Signed-off-by: randomhydrosol <randomhydrosol@glassrom.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -83,10 +83,10 @@
 #endif
 
 /* shared constants to be used in various sysctls */
-const int sysctl_vals[] = { 0, 1, 2, 3, 4, 100, 200, 1000, 3000, INT_MAX, 65535, -1 };
+const int sysctl_vals[] __read_only = { 0, 1, 2, 3, 4, 100, 200, 1000, 3000, INT_MAX, 65535, -1 };
 EXPORT_SYMBOL(sysctl_vals);
 
-const unsigned long sysctl_long_vals[] = { 0, 1, LONG_MAX };
+const unsigned long sysctl_long_vals[] __read_only = { 0, 1, LONG_MAX };
 EXPORT_SYMBOL_GPL(sysctl_long_vals);
 
 #if defined(CONFIG_SYSCTL)
@@ -94,12 +94,12 @@ EXPORT_SYMBOL_GPL(sysctl_long_vals);
 /* Constants used for minimum and maximum */
 
 #ifdef CONFIG_PERF_EVENTS
-static const int six_hundred_forty_kb = 640 * 1024;
+static const int six_hundred_forty_kb __read_only = 640 * 1024;
 #endif
 
 
-static const int ngroups_max = NGROUPS_MAX;
-static const int cap_last_cap = CAP_LAST_CAP;
+static const int ngroups_max __read_only = NGROUPS_MAX;
+static const int cap_last_cap __read_only = CAP_LAST_CAP;
 
 #ifdef CONFIG_PROC_SYSCTL
 
