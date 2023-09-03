95151d9cf3aa0250d513dd988594f9eebcb2d40d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-09-03 17:33:23 +0300

    selinux: cache zygote context types when loading policy
    They are used by the next commit.

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -89,6 +89,11 @@ extern int selinux_enabled_boot;
 struct selinux_avc;
 struct selinux_policy;
 
+struct context_types {
+	u32 webview_zygote;
+	u32 zygote;
+};
+
 struct selinux_state {
 #ifdef CONFIG_SECURITY_SELINUX_DISABLE
 	bool disabled;
@@ -101,6 +106,8 @@ struct selinux_state {
 	bool policycap[__POLICYDB_CAP_MAX];
 	bool android_netlink_route;
 	bool android_netlink_getneigh;
+
+	struct context_types types;
 
 	struct page *status_page;
 	struct mutex status_lock;
