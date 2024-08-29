171c4aed4cd9d3d34c6b12bd2283ea0f127f364e
Daniel Micay <daniel.micay@grapheneos.org>
2024-08-29 03:31:38 -0400

    use deterministic RANDSTRUCT seed based on timestamp

--- scripts/gen-randstruct-seed.sh
+++ scripts/gen-randstruct-seed.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0
 
-SEED=$(od -A n -t x8 -N 32 /dev/urandom | tr -d ' \n')
+SEED=$(echo -n $KBUILD_BUILD_TIMESTAMP | sha256sum | cut -f 1 -d ' ')
 echo "$SEED" > "$1"
 HASH=$(echo -n "$SEED" | sha256sum | cut -d" " -f1)
 echo "#define RANDSTRUCT_HASHED_SEED \"$HASH\"" > "$2"
