43e2d5837234a0f4687ce5d263003d0057242a0a
liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>
2026-08-14 14:33:47 +0800

    ANDROID: add vendor hooks for f2fs iostat monitoring
    Add vendor hooks to allow external modules to implement I/O latency
    monitoring.
    
    Bug: 542535040
    Change-Id: I08edb25fd8eeec549fdcf7a00d5b96950a93e8b6
    Signed-off-by: liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -28,6 +28,7 @@
 #include "segment.h"
 #include "iostat.h"
 #include <trace/events/f2fs.h>
+#include <trace/hooks/fs.h>
 
 #define NUM_PREALLOC_POST_READ_CTXS	128
 
@@ -530,6 +531,7 @@ void f2fs_submit_read_bio(struct f2fs_sb_info *sbi, struct bio *bio,
 	trace_f2fs_submit_read_bio(sbi->sb, type, bio);
 
 	iostat_update_submit_ctx(bio, type);
+	trace_android_vh_f2fs_iostat_submit(sbi->sb, type, bio);
 	submit_bio(bio);
 }
 
@@ -580,6 +582,7 @@ static void f2fs_submit_write_bio(struct f2fs_sb_info *sbi, struct bio *bio,
 
 	trace_f2fs_submit_write_bio(sbi->sb, type, bio);
 	iostat_update_submit_ctx(bio, type);
+	trace_android_vh_f2fs_iostat_submit(sbi->sb, type, bio);
 	submit_bio(bio);
 }
 
--- include/trace/hooks/fs.h
+++ include/trace/hooks/fs.h
@@ -29,6 +29,14 @@ DECLARE_HOOK(android_vh_erofs_iostat_submit,
 DECLARE_HOOK(android_vh_erofs_iostat_update,
 	TP_PROTO(struct super_block *sb, struct bio *bio),
 	TP_ARGS(sb, bio));
+
+DECLARE_HOOK(android_vh_f2fs_iostat_submit,
+	TP_PROTO(struct super_block *sb, int type, struct bio *bio),
+	TP_ARGS(sb, type, bio));
+
+DECLARE_HOOK(android_vh_f2fs_iostat_update,
+	TP_PROTO(struct super_block *sb, struct bio *bio, bool *skip),
+	TP_ARGS(sb, bio, skip));
 #endif /* _TRACE_HOOK_FS_H */
 
 /* This part must be outside protection */
